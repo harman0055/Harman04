4fd4d936104a520afc685e4e26976352b7514ffd
harman0055 <harm0055@algonquinlive.com>
2024-02-17 01:24:25 -0500

    till the opening of second page

--- app/src/main/java/com/example/harman04/MainActivity.java
+++ app/src/main/java/com/example/harman04/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.harman04;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button Button = findViewById(R.id.button);
+
+        Button.setOnClickListener(clk -> {
+            // Handle the button click
+            Intent nextPage = new Intent(MainActivity.this, Secondactivity.class);
+            startActivity(nextPage);
+        });
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w( "MainActivity", "Application is now visible on screen" );
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.w( "MainActivity", "Application is now responding to the user input" );
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
+
+    }
+
 }
